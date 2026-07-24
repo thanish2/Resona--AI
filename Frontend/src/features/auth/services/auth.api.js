fee3856b47ad6191fc7d6f4c526d2c32059ecb12
Thanish P M <thanipm20@gmail.com>
2026-07-24 12:31:30 +0530

    fix: update API base URL to use environment variable

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const api=axios.create({
-    baseURL:'http://localhost:3000',
-    withCredentials:true
-})
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
 
 export async function register({username,email,password}){
     try{
