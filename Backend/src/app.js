d73e9e9aca1aab45b2dccf064f0b3513c1b25854
Thanish P M <thanipm20@gmail.com>
2026-07-24 12:37:38 +0530

    fix: update CORS configuration to use environment variable and enhance cookie security settings

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,9 +13,9 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 
 // using all the routes
 app.use("/api/auth",authRouter)
--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -78,7 +78,12 @@ async function loginUserController(req,res){
       { expiresIn: "1d" },
     );
 
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
 
     res.status(200).json({
       message: "User logged in successfully",
@@ -103,7 +108,11 @@ async function logoutUserController(req,res){
         
     }
 
-    res.clearCookie("token")
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
 
     res.status(200).json({message:"User logged out successfully"})
 }
